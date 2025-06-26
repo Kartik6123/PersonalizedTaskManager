c8b61c9ab37b2f2fac664c80f0c398f372a3b1b1
Your Name <youremail@example.com>
2025-06-26 09:18:49 +0530

    Connected to MongoDB Atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
-export const connectDB= async()=>{
-    try {
-		await mongoose.connect("mongodb://localhost:27017/mydatabase");
-		console.log("Connected to MongoDB");
-	} catch (error) {
-		console.log("Error connecting to MongoDB", error.message);
-	}
-}
- 
+export const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://kartikarora29032004:pass123@cluster0.eqs9jva.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    console.log("✅ Connected to MongoDB Atlas");
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB:", error.message);
+  }
+};
